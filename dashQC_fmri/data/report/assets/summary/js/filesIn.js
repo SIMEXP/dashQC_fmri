d299434425cc98d52c2394674dd5af492cbd1bd0
surchs <sebastian.urchs@gmail.com>
2018-09-27 00:30:41 -0400

    provenance templates for fmriprep until we have a complete solution in place

---
+++ dashQC_fmri/data/report/assets/summary/js/filesIn.js
@@ -0,0 +1,11 @@
+function buildFilesIn (evt) {
+
+	var filesIn = {
+			"fmriprep_provenance_information": "coming soon, this is a placeholder"
+		}
+	return filesIn
+}
+
+var opt = {
+	"fmriprep_provenance_information": "coming soon, this is a placeholder"
+}
---
+++ dashQC_fmri/data/report/assets/summary/js/pipeSummary.js
@@ -0,0 +1,1 @@
+var pipeSummary = '<h1>This is a provenance report for fmriprep</h1>Coming soon. This is a placeholder.';
