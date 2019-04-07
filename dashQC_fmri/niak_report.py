526d082a59ab61923566f88cc34a017ff8290ce1
surchs <sebastian.urchs@gmail.com>
2019-04-07 13:28:44 -0400

    Remove glob and OS dependencies - deal with paths that don't end with a path separator - deal with wildcards within pathlib - remove deprecated folder handling functions - make sure passed paths are pathlib type

--- dashQC_fmri/niak_report.py
+++ dashQC_fmri/niak_report.py
@@ -6,10 +6,8 @@ Script for copying NIAK fMRI preprocessing report output into new folder structu
 
 """
 
-import os
 import time
 import json
-import glob
 import shutil
 import inspect
 import argparse
@@ -43,19 +41,11 @@ def populate_report(report_p):
     return
 
 
-def clean_folder(p_folder):
-
-    cleaned_folder = p_folder.strip()
-    if cleaned_folder[len(cleaned_folder) - 1] != os.sep:
-        cleaned_folder += os.sep
-    return cleaned_folder
-
-
 def copy_all_files(p_src_folder_wildcard, p_dest_folder):
 
     print("...{0}".format(p_dest_folder))
 
-    for file in glob.glob(p_src_folder_wildcard):
+    for file in p_src_folder_wildcard.parent.glob(p_src_folder_wildcard.name):
         if copy_debug:
             print("Copying {0} to {1}".format(file, p_dest_folder))
         shutil.copy(file, p_dest_folder)
@@ -66,19 +56,17 @@ def create_dataset_ids(p_dataset_id_folder):
     # Date and/or timestamp will be used by dashQC as unique identifiers for a data set
     # The assumption here is that it is highly unlikely that two data sets will conflict
     # based on this distinction criteria
-    with open(p_dataset_id_folder + os.sep + "datasetID.js", "w") as data_id_file:
+    with (p_dataset_id_folder / "datasetID.js").open("w") as data_id_file:
         data_id_json = { "date": time.strftime("%Y-%m-%d-%H:%M:%S"),
                          "timestamp": time.time() }
         data_id_file.write("var datasetID = " + json.dumps(data_id_json) + ";")
 
 
-def create_folder(p_new_folder):
-
-    if not os.path.exists(p_new_folder):
-        os.makedirs(p_new_folder)
-
-
 def make_report(preproc_dir, report_dir):
+    if not issubclass(type(preproc_dir), pal.Path):
+        preproc_dir = pal.Path(preproc_dir)
+    if not issubclass(type(report_dir), pal.Path):
+        report_dir = pal.Path(report_dir)
 
     print("Conversion of old NIAK QC dashboard folder structure to new one commencing...")
 
@@ -91,36 +79,36 @@ def make_report(preproc_dir, report_dir):
     print("Copying files...")
     # group/*.png -> assets/group/images
     # group/*.js -> assets/group/js
-    copy_all_files(preproc_dir + "group{0}*.png".format(os.sep),
-                   report_dir + "assets{0}group{0}images".format(os.sep))
-    copy_all_files(preproc_dir + "group{0}*.js".format(os.sep),
-                   report_dir + "assets{0}group{0}js".format(os.sep))
+    copy_all_files(preproc_dir / "group/*.png",
+                   report_dir / "assets/group/images")
+    copy_all_files(preproc_dir / "group{0}*.js",
+                   report_dir / "assets/group/js")
 
     # motion/*.html -> assets/motion/html
     # motion/*.png -> assets/motion/images
     # motion/*.js -> assets/motion/js
     #copy_all_files(preproc_dir + "motion{0}*.html".format(os.sep),
     #               report_dir + "assets{0}motion{0}html".format(os.sep))
-    copy_all_files(preproc_dir + "motion{0}*.png".format(os.sep),
-                   report_dir + "assets{0}motion{0}images".format(os.sep))
-    copy_all_files(preproc_dir + "motion{0}*.js".format(os.sep),
-                   report_dir + "assets{0}motion{0}js".format(os.sep))
+    copy_all_files(preproc_dir / "motion/*.png",
+                   report_dir / "assets/motion{0}images")
+    copy_all_files(preproc_dir / "motion/*.js",
+                   report_dir / "assets/motion/js")
 
     # qc_registration.csv -> assets/registration/csv
     # registration/*.png -> assets/registration/images
-    copy_all_files(preproc_dir + "qc_registration.csv".format(os.sep),
-                   report_dir + "assets{0}registration{0}csv".format(os.sep))
-    copy_all_files(preproc_dir + "registration{0}*.png".format(os.sep),
-                   report_dir + "assets{0}registration{0}images".format(os.sep))
+    copy_all_files(preproc_dir / "qc_registration.csv",
+                   report_dir / "assets/registration/csv")
+    copy_all_files(preproc_dir / "registration/*.png",
+                   report_dir / "assets/registration/images")
 
     # summary/*.js -> assets/summary/js
-    copy_all_files(preproc_dir + "summary{0}*.js".format(os.sep),
-                   report_dir + "assets{0}summary{0}js".format(os.sep))
+    copy_all_files(preproc_dir / "summary/*.js",
+                   report_dir / "assets/summary/js")
 
     # (3) Create a JSON file for this conversion session that registers this as a unique data set for the dashQC
 
     print("Creating unique IDs for this data set...")
-    create_dataset_ids(report_dir + "assets{0}registration{0}js".format(os.sep))
+    create_dataset_ids(report_dir / "assets/registration/js")
  
     print("Conversion complete.")
 
